343cb7eec5b5bc1fff70d607b70245a2208dcb81
Skrrrrra <flsab45@gmail.com>
2022-05-26 20:00:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SummOfPow2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //путь
+            string inputpath = "D:\\SolutionsForSpaceApp\\2009\\input.txt";
+            string outputpath = "D:\\SolutionsForSpaceApp\\2009\\output.txt";
+
+            //создание файлов
+            FileStream fs = new FileStream(inputpath, FileMode.OpenOrCreate);
+            fs.Close();
+            FileStream fsout = new FileStream(outputpath, FileMode.OpenOrCreate);
+            fsout.Close();
+
+
+            //запись в строку содержимого строки файла
+            string secondLine;
+            using (var reader = new StreamReader(inputpath))
+            {
+                secondLine = reader.ReadLine();  // записываем в переменную
+            }
+
+
+            string[] secondlineforint = secondLine.Split(' ');
+
+            //массив для действий над числами
+            int[] lastintarr;
+            lastintarr = new int[2];
+
+            //запись в интовый массив из массива строк
+            int count = 0;
+            foreach (var s in secondlineforint)
+            {
+                lastintarr[count] = Convert.ToInt32(s);
+                count++;
+            }
+
+
+
+            int m = lastintarr[0];
+            int n = lastintarr[1];
+            int counter = 0;
+            int nod;
+            while (n > 0 && m > 0)
+            {
+                if (m == n)
+                {
+                    nod = m;
+                }
+
+                if (n > m)
+                {
+                    n -= m;
+                }
+                else
+                {
+                    m -= n;
+                }
+                counter++;
+            }
+            nod = n;
+            string output = counter + " " + nod;
+            File.WriteAllText(outputpath, output);
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Nod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Nod")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fe73e2fc-de2c-49b4-a350-2bc78ddbb99a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
